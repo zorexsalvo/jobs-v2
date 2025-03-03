1948890857ae9defd766f326248741fa2cb179ee
poypoyan <srazin123@gmail.com>
2025-03-03 13:40:38 +0800

    Admin view initial customization (#25)

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Job
 
-admin.site.register(Job)
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('title', 'company_name', 'location', 'salary_range', 'is_remote')
+    search_fields = ('title', 'company_name', 'location')
