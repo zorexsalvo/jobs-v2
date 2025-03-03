6e50b3d3f0fa51a7e51e6f0b940c4802345f9ddd
ThisPC <srazin123@gmail.com>
2025-03-03 12:21:51 +0800

    Move content to content.html

--- jobs/views.py
+++ jobs/views.py
@@ -1,7 +1,9 @@
 from django.views.generic import ListView 
 from django.contrib.auth.models import User
+from django.shortcuts import render
+from . import models
 
-
-class JobIndexView(ListView):
-    model = User
-    template_name = "index.html"
+def populate_content(request):
+    job_query = models.Job.objects.all()
+    print(job_query)
+    return render(request, 'content.html', { 'jobs': job_query })
