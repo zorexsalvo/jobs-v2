eb59a6da1f2a1f7b02f245629b4a20391b57eede
Zorex Salvo <zorexsalvo@gmail.com>
2025-03-04 08:37:56 +0800

    feat: Enhance job search and detail views with advanced features (#28)
    - Add search functionality to job listings
    - Implement job detail view with related jobs
    - Update docker-compose and settings for PostgreSQL
    - Add django-extensions to dev requirements
    - Improve job listing template with search and detail links
    - Create new job detail template with comprehensive job information

--- jobs/views.py
+++ jobs/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.db.models import Q
 from .models import Job
 
 
@@ -7,7 +8,32 @@ class JobIndex(ListView):
     template_name = "content.html"
     paginate_by = 10
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_query = self.request.GET.get('search', '')
+        if search_query:
+            queryset = queryset.filter(
+                Q(title__icontains=search_query) |
+                Q(company_name__icontains=search_query)
+            )
+        return queryset
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["count"] = self.get_queryset().count()
+        context["job_count"] = self.get_queryset().count()
+        context["company_count"] = Job.objects.values('company_name').distinct().count()
+        context["search_query"] = self.request.GET.get('search', '')
+        return context
+
+class JobDetail(DetailView):
+    model = Job
+    template_name = "job_detail.html"
+    context_object_name = 'job'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Add related jobs from the same company
+        context["related_jobs"] = Job.objects.filter(
+            company_name=self.object.company_name
+        ).exclude(id=self.object.id)[:3]
         return context
--- jobs_v2/settings.py
+++ jobs_v2/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'jobs_v2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': '5432',
     }
 }
 
